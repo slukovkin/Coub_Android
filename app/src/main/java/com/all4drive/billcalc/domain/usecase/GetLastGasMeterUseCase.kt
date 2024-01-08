1adcf96d1ee283da7056526fd308f6de4a20ce59
Sergey Lukovkin <slukovkin@icloud.com>
2024-01-08 23:55:08 +0200

    update domain layer

--- app/src/main/java/com/all4drive/billcalc/domain/usecase/GetLastGasMeterUseCase.kt
+++ app/src/main/java/com/all4drive/billcalc/domain/usecase/GetLastGasMeterUseCase.kt
@@ -2,8 +2,8 @@ package com.all4drive.billcalc.domain.usecase
 
 import com.all4drive.billcalc.domain.models.Meter
 
-class GetLastMeterUseCase {
-    fun getLastMeter(param: String): Meter {
+class GetLastGasMeterUseCase {
+    fun getLastMeter(): Meter {
         TODO()
     }
 }
