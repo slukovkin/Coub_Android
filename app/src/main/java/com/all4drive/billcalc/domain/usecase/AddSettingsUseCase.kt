eb8d8836322759da53802a32533afd4d7c7691b1
Sergey Lukovkin <slukovkin@icloud.com>
2024-01-08 23:34:33 +0200

    added repositories on domain layers

--- app/src/main/java/com/all4drive/billcalc/domain/usecase/AddSettingsUseCase.kt
+++ app/src/main/java/com/all4drive/billcalc/domain/usecase/AddSettingsUseCase.kt
@@ -1,4 +1,9 @@
 package com.all4drive.billcalc.domain.usecase
 
+import com.all4drive.billcalc.domain.models.Setting
+
 class AddSettingsUseCase {
+    fun assSettings(setting: Setting): Boolean {
+        TODO()
+    }
 }
